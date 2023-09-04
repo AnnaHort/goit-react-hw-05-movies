f1a17dea056fa1a631c1cba8c322a4fddf3d0f8d
AnnaHort <annahort3d@gmail.com>
2023-09-04 12:27:41 +0300

    realized search query

--- src/components/Pages/MoviesSearch/MoviesSearchPages.js
+++ src/components/Pages/MoviesSearch/MoviesSearchPages.js
@@ -5,18 +5,30 @@ import {
   SearchForm,
 } from './MoviesSearchPages.style';
 import { useState } from 'react';
+import { getMovies } from 'components/API';
+import { Link } from 'react-router-dom';
 
 export const MoviesSearch = () => {
+  const adress = '/search/movie';
   const [inputValue, setInputValue] = useState('');
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
-  const handleSubmit = e => {
+
+  const handleSubmit = async e => {
     e.preventDefault();
-    if (inputValue === '' || inputValue === null || inputValue === undefined) {
+    if (!inputValue) {
       return alert('You did not write any movie :(');
     }
-    console.log(inputValue);
+
+    try {
+      const moviesData = await getMovies(`${adress}?query=${inputValue}`);
+      setTrendingMovies(moviesData.results);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
     e.target.reset();
   };
+
 
   const handleInputChange = e => {
     setInputValue(e.target.value);
@@ -24,79 +36,30 @@ export const MoviesSearch = () => {
 
   return (
     <>
-     <SearchForm onSubmit={handleSubmit}>
-      <InputStyled
-        className="input"
-        type="text"
-        name="filter"
-        placeholder="Search"
-        onChange={handleInputChange}
-      ></InputStyled>
-      <SearchButtonStyled type="submit">
-        <BiSearchAlt2 />
-      </SearchButtonStyled>
-    </SearchForm>
-    <ul>
-      <li>this is a place for movie</li>
-      <li>this is a place for movie</li>
-      <li>this is a place for movie</li>
-      <li>this is a place for movie</li>
-    </ul>
+      <SearchForm onSubmit={handleSubmit}>
+        <InputStyled
+          className="input"
+          type="text"
+          name="filter"
+          placeholder="Search"
+          onChange={handleInputChange}
+        ></InputStyled>
+        <SearchButtonStyled type="submit">
+          <BiSearchAlt2 />
+        </SearchButtonStyled>
+      </SearchForm>
+      <ul>
+        {trendingMovies.map(movie => {
+          if (!movie.title) {
+            return null;
+          }
+          return (
+            <li key={movie.id}>
+              <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { BiSearchAlt2 } from 'react-icons/bi';
-// import {
-//   SearchButtonStyled,
-//   InputStyled,
-//   SearchForm,
-// } from './MoviesSearchPages.style';
-// import { useState } from 'react';
-// import { SearchMovies } from '../SearchBar/SearchBar';
-
-// export const MoviesSearch = () => {
-//   const [inputValue, setInputValue] = useState('');
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     if (inputValue === '' || inputValue === null || inputValue === undefined) {
-//       return alert('You did not write any movie :(');
-//     }
-//     console.log(inputValue);
-//     e.target.reset();
-//   };
-
-//   const handleInputChange = e => {
-//     setInputValue(e.target.value);
-//   };
-
-//   return (
-//     <>
-//      <SearchForm onSubmit={handleSubmit}>
-//       <InputStyled
-//         className="input"
-//         type="text"
-//         name="filter"
-//         placeholder="Search"
-//         onChange={handleInputChange}
-//       ></InputStyled>
-//       <SearchButtonStyled type="submit">
-//         <BiSearchAlt2 />
-//       </SearchButtonStyled>
-//     </SearchForm>
-//     </>
-//   );
-// };
